ec7d35998e77ed243b3382198bd56a79f528fadd
RawSaurus <mirino.bella@gmail.com>
2024-12-09 15:15:59 +0100

    angular update

--- activity-tracker-ui/src/app/app.module.ts
+++ activity-tracker-ui/src/app/app.module.ts
@@ -5,29 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import {FormsModule} from '@angular/forms';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import {CodeInputModule} from 'angular-code-input';
 import { RegisterComponent } from './pages/register/register.component';
 import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    ActivateAccountComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    CodeInputModule
-  ],
-  providers: [
-    provideClientHydration(),
-    HttpClient
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        LoginComponent,
+        RegisterComponent,
+        ActivateAccountComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        CodeInputModule], providers: [
+        provideClientHydration(),
+        HttpClient,
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
